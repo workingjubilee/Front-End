d9d1034e75515305a413b24942b7a21a14d0f01c
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-04-24 16:18:03 -0500

    Adding skeleton views Journal, Login, and SignUp.

---
+++ src/views/Journal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Journal = () => {
+  return (
+    <div className="journalPage">
+      <h2>Your medical journal!</h2>
+    </div>
+  );
+};
+
+export default Journal;
---
+++ src/views/SignUp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="signUpPage">
+      <form>
+        <input type="text" />
+        <input type="password" />
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
