9176e999453955638579220897919f57f2c906c8
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-04-24 15:04:20 -0500

    sample reducer in place. Basic folder structure is laid out

--- rx-id/src/App.js
+++ rx-id/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Rx iD</h1>
+        <h2>Identify some medication</h2>
       </header>
     </div>
   );
