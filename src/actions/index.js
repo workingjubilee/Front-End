468d1c35b48b803030dcef54617b8c499b699cf4
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-25 19:30:29 -0400

    Wrote out an action creator for retrieving user info by ID.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,28 @@
+require("dotenv").config();
+import axios from "axios";
+const endpoint = process.env.REACT_APP_ENDPOINT;
+
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 
 export const login = creds => dispatch => {
   dispatch({ type: LOGIN_SUCCESS });
+};
+
+export const FETCH_USER = "FETCH_USER";
+export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
+export const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
+
+export const getUser = id => dispatch => {
+  dispatch({ type: FETCH_USER });
+  axios
+    .get(`${endpoint}/api/users/${id}`)
+    .then(res => {
+      dispatch({ type: FETCH_USER_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({
+        type: FETCH_USER_FAILURE,
+        payload: `${err.message}. ${err.response.data.message}`
+      });
+    });
 };
