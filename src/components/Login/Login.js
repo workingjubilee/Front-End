3c21129fd5c5a9b738e857ebfbe73b97f51dfd09
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-25 19:16:18 -0400

    Added input fields for Login component. Used useInput Hooks utility.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { useInput } from '../../utilities/useInput';
 
 const Login = () => {
+  const username = useInput();
+  const password = useInput();
+
+  // login action creator call goes here...
+
   return (
-    <div className="loginPage">
+    <div className='loginPage'>
       <form>
-        <input type="text" />
-        <input type="password" />
+        <input
+          required
+          type='text'
+          value={username.value}
+          name='username'
+          onChange={username.updateValue}
+          placeholder='username'
+        />
+        <input
+          type='password'
+          value={password.value}
+          name='password'
+          onChange={password.updateValue}
+          placeholder='password'
+        />
+        <button type='submit'>Login</button>
       </form>
     </div>
   );
 };
 
-export default Login;
+export default connect(
+  null,
+  null
+)(Login);
