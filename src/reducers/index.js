53aae05b265ec8831b44cbafbbea4fad14a6a2fe
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-25 19:48:12 -0400

    Reducer cases for fetching user added. Added some fields to initialState.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,40 @@
 // import actions
-import { LOGIN_SUCCESS } from "../actions";
+import {
+  LOGIN_SUCCESS,
+  FETCH_USER,
+  FETCH_USER_SUCCESS,
+  FETCH_USER_FAILURE
+} from "../actions";
 
-const initialState = {};
+const initialState = {
+  loggingIn: false,
+  fetchingUser: false,
+  user: {},
+  error: null
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_SUCCESS:
       return { ...state, loggingIn: false };
+    case FETCH_USER:
+      return {
+        ...state,
+        fetchingUser: true,
+        error: null
+      };
+    case FETCH_USER_SUCCESS:
+      return {
+        ...state,
+        fetchingUser: false,
+        user: action.payload
+      };
+    case FETCH_USER_FAILURE:
+      return {
+        ...state,
+        fetchingUser: false,
+        error: action.payload
+      };
     default:
       return state;
   }
