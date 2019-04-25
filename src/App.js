b4a469986956813656f35493f084c406a980f26d
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-25 19:34:07 -0400

    Per web group decision, changed Journal to Diary.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Route, NavLink } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import './App.css';
 
-import Journal from "./components/Journal/Journal";
-import Login from "./components/Login/Login";
-import Scan from "./components/Scan/Scan";
-import SignUp from "./components/SignUp/SignUp";
-import UserDashboard from "./components/UserDashboard/UserDashboard";
-import Landing from "./components/Landing/Landing";
+import Diary from './components/Diary/Diary';
+import Login from './components/Login/Login';
+import Scan from './components/Scan/Scan';
+import SignUp from './components/SignUp/SignUp';
+import UserDashboard from './components/UserDashboard/UserDashboard';
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/landing" component={Landing} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={SignUp} />
-        <Route exact path="/journal" component={Journal} />
-        <Route exact path="/scan" component={Scan} />
-        <Route exact path="/dashboard" component={UserDashboard} />
+    <div className='App'>
+      <header className='App-header'>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/landing' component={Landing} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={SignUp} />
+        <Route exact path='/diary' component={Diary} />
+        <Route exact path='/scan' component={Scan} />
+        <Route exact path='/dashboard' component={UserDashboard} />
       </header>
     </div>
   );
@@ -27,11 +27,11 @@ function App() {
 
 function Home() {
   return (
-    <div className="homeScreen">
+    <div className='homeScreen'>
       <h2>Home screen</h2>
-      <NavLink to="/login">Login</NavLink>
+      <NavLink to='/login'>Login</NavLink>
       <br />
-      <NavLink to="/register">Register</NavLink>
+      <NavLink to='/register'>Register</NavLink>
     </div>
   );
 }
