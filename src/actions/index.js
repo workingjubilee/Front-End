4516a812d23a1f528ff589392dbf340a595a418e
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-25 23:10:50 -0400

    Action creator for registering a user added.

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,6 +23,27 @@ export const logIn = creds => dispatch => {
     });
 };
 
+export const SIGNUP_USER = "SIGNUP_USER";
+export const SIGNUP_USER_SUCCESS = "SIGNUP_USER_SUCCESS";
+export const SIGNUP_USER_FAILURE = "SIGNUP_USER_FAILURE";
+
+export const signUp = creds => dispatch => {
+  dispatch({ type: SIGNUP_USER });
+  return axios
+    .post(`${endpoint}/api/auth/register`, creds)
+    .then(res => {
+      dispatch({ type: SIGNUP_USER_SUCCESS });
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("userID", res.data.id);
+    })
+    .catch(err => {
+      dispatch({
+        type: SIGNUP_USER_FAILURE,
+        payload: `${err.message}. ${err.response.data.message}`
+      });
+    });
+};
+
 export const FETCH_USER = "FETCH_USER";
 export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
 export const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
