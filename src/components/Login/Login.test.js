740971cdbf595848ffa5c597f74046e72b979683
workingjubilee <workingjubilee@gmail.com>
2019-04-25 23:18:40 -0700

    Testing is good (@__@) Adds Redux component smoke tests

---
+++ src/components/Login/Login.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './Login';
+import { Provider } from "react-redux";
+import store from "../../store";
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Provider store={store}><Login /></Provider>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
