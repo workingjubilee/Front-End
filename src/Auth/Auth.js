a9be963a238ff5a17f64bac55b2407baf507a1ae
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-04-30 12:47:15 -0500

    change redirectURL

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -7,16 +7,12 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'rxid.auth0.com',
     clientID: process.env.AUTH0_CLIENT_ID,
-    redirectUri: 'http://localhost:3000/callback',
+    redirectUri: 'http://localhost:3000/dashboard',
     responseType: 'token id_token',
     scope: 'openid'
   });
 
-  //   constructor() {
-  //       this.login =
-  //   }
-
-  login() {
+  login = () => {
     this.auth0.authorize();
-  }
+  };
 }
