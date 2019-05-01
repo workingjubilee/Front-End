ccd62a407a67af2da81169a0082007a7437f7b1e
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-05-01 18:13:31 -0500

    puts auth in redux store Co-authored-by: jacob jacobangulo@hotmail.com

--- src/App.js
+++ src/App.js
@@ -11,25 +11,19 @@ import Landing from './components/Landing/Landing';
 import Callback from './components/Callback/Callback';
 import Loading from './components/Loading/Loading';
 
-
-
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Route exact path='/' component={Home} />
         <Route exact path='/landing' component={Landing} />
-        <Route exact path='/login' render={props => <Login {} />} />
+        <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
         <Route exact path='/diary' component={Diary} />
         {/* <Route exact path='/scan' component={Scan} /> */}
         <Route exact path='/dashboard' component={Dashboard} />
         <Route exact path='/callback' component={Callback} />
-        <Route
-          exact
-          path='/loading'
-          render={props => <Loading {...props} auth={auth} />}
-        />
+        <Route exact path='/loading' component={Loading} />
       </header>
     </div>
   );
