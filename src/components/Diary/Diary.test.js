506059f3ef2deabe2649ea25aa54d30a8511ea14
workingjubilee <workingjubilee@gmail.com>
2019-05-01 16:21:06 -0700

    Fixes tests

--- src/components/Diary/Diary.test.js
+++ src/components/Diary/Diary.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Diary from './Diary';
+import { Provider } from 'react-redux';
+import store from '../../store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Diary />, div);
+  ReactDOM.render(<Provider store={store}><Diary /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/Meds/Meds.test.js
+++ src/components/Meds/Meds.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MedList from './MedList';
+import Meds from './Meds';
+import { Provider } from 'react-redux';
+import store from '../../store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<MedList />, div);
+  ReactDOM.render(<Provider store={store}><Meds /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
