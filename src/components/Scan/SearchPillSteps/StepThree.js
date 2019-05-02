57989e0e0449bdc8cb1e032c8ad3c8019a5c3728
Jacob Angulo <jacobangulo@hotmail.com>
2019-05-02 14:56:05 -0700

    added comments describing what the component should do

--- src/components/Scan/SearchPillSteps/StepThree.js
+++ src/components/Scan/SearchPillSteps/StepThree.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const StepThree = () => {
+  useEffect(() => {
+    console.log('step 3')
+    // populate fields with data we get back from ds
+  });
   return (
     <div>
       <p>results</p>
