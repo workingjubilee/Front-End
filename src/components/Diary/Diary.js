b05dd080d1f9df3d97feae42e47e8395667356b1
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-05-07 19:36:26 -0400

    Working on Diary component.

--- src/components/Diary/Diary.js
+++ src/components/Diary/Diary.js
@@ -2,32 +2,41 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchDiary } from '../../actions';
-import DiaryEntry from '../Diary/DiaryEntry';
+// import DiaryEntry from '../Diary/DiaryEntry';
+import DiaryMed from './DiaryMed';
 
-const Diary = ({ fetchDiary, user_id, diary }) => {
+const Diary = ({ fetchDiary, user_id, diary, meds, fetchingMeds }) => {
   useEffect(() => {
     fetchDiary(user_id);
   }, [user_id, fetchDiary]);
 
   return (
     <div className='diary'>
-      {diary.length > 0 ? (
+      {fetchingMeds === true ? (
+        <h2>Loading medications...</h2>
+      ) : meds.length > 0 ? (
         <div>
           <h2>Diary:</h2>
-          {diary.map(diaryEntry => (
-            <DiaryEntry key={diaryEntry.id} diaryEntry={diaryEntry} />
+          <h2>Meds</h2>
+          {meds.map(med => (
+            <DiaryMed key={med.id} med_name={med.med_name} />
           ))}
         </div>
       ) : (
-        <h2>Loading Diary...</h2>
+        <h2>
+          You aren't tracking any medications. You need to track a medication to
+          record a diary entry.
+        </h2>
       )}
     </div>
   );
 };
 
 const mapStateToProps = state => ({
+  meds: state.meds,
   diary: state.diary,
   fetchingDiary: state.fetchingDiary,
+  fetchingMeds: state.fetchingMeds,
   error: state.error
 });
 
---
+++ src/components/Diary/DiaryMed.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    minWidth: 275,
+    margin: 10
+  },
+  title: {
+    fontSize: 100
+  }
+};
+
+const DiaryMed = ({ classes, med_name }) => {
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title}>{med_name}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant='contained' color='primary'>
+          Diary Entries
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default withStyles(styles)(DiaryMed);
