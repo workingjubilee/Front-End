8bc6543a3bb28180eda16df79b55c35a0b1bdeef
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-05-07 22:51:58 -0400

    Further Diary component refactoring.

--- src/components/Diary/Diary.js
+++ src/components/Diary/Diary.js
@@ -5,40 +5,26 @@ import { fetchDiary } from '../../actions';
 // import DiaryEntry from '../Diary/DiaryEntry';
 import DiaryMeds from './DiaryMeds';
 
-const Diary = ({ fetchDiary, user_id, diary, meds, fetchingMeds }) => {
+const Diary = ({ fetchDiary, user_id }) => {
   useEffect(() => {
     fetchDiary(user_id);
   }, [user_id, fetchDiary]);
 
   return (
-    <div>
-      {fetchingMeds === true ? (
-        <h2>Loading medications...</h2>
-      ) : meds.length === 0 ? (
-        <h2>
-          You aren't tracking any medications. You need to track a medication to
-          record a diary entry.
-        </h2>
-      ) : (
-        <div className='diary'>
-          <div className='diaryTitle'>
-            <h1>Diary:</h1>
-          </div>
-          <div className='diaryBody'>
-            <DiaryMeds className='diaryMeds' />
-            <h2>Diary Entries</h2>
-          </div>
-        </div>
-      )}
+    <div className='diary'>
+      <div className='diaryTitle'>
+        <h1>Diary:</h1>
+      </div>
+      <div className='diaryBody'>
+        <DiaryMeds className='diaryMeds' />
+        <h2>Diary Entries</h2>
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = state => ({
-  meds: state.meds,
-  diary: state.diary,
   fetchingDiary: state.fetchingDiary,
-  fetchingMeds: state.fetchingMeds,
   error: state.error
 });
 
--- src/components/Diary/DiaryMeds.js
+++ src/components/Diary/DiaryMeds.js
@@ -3,18 +3,30 @@ import { connect } from 'react-redux';
 
 import DiaryMed from './DiaryMed';
 
-const DiaryMeds = ({ meds }) => {
+const DiaryMeds = ({ fetchingMeds, meds }) => {
   return (
     <div>
-      <h2>Meds</h2>
-      {meds.map(med => (
-        <DiaryMed key={med.id} med_name={med.med_name} />
-      ))}
+      {fetchingMeds === true ? (
+        <h2>Loading medications...</h2>
+      ) : meds.length === 0 ? (
+        <h2>
+          You aren't tracking any medications. You need to track a medication to
+          record a diary entry.
+        </h2>
+      ) : (
+        <div>
+          <h2>Meds</h2>
+          {meds.map(med => (
+            <DiaryMed key={med.id} med_name={med.med_name} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
 const mapStateToProps = state => ({
+  fetchingMed: state.fetchingMeds,
   meds: state.meds
 });
 
