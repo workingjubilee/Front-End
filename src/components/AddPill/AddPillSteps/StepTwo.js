21dc88e4db963ce5724cad18529478fd92da0ed3
Jacob Angulo <jacobangulo@hotmail.com>
2019-05-08 08:53:43 -0700

    incomplete StepTwo push to resolve merge conflicts

--- src/components/AddPill/AddPillSteps/StepTwo.js
+++ src/components/AddPill/AddPillSteps/StepTwo.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Fab from '@material-ui/core/Fab';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 
-const StepTwo = () => {
+const StepTwo = props => {
+  const incrementCapsulesPerDose = () => {
+    props.updateCapsulesPerDose(props.capsulesPerDose + 1);
+  };
+  const decrementCapsulesPerDose = () => {
+    props.updateCapsulesPerDose(props.capsulesPerDose - 1);
+  };
   return (
-    <div>
-      <p>step 2</p>
-    </div>
-  )
-}
+    <form>
+      <Card>
+        <Fab onClick={decrementCapsulesPerDose}>
+          <RemoveIcon />
+        </Fab>
 
-export default StepTwo
+        <Typography component='h2' variant='display4' gutterBottom>
+          {props.capsulesPerDose}
+        </Typography>
+
+        <Fab onClick={incrementCapsulesPerDose}>
+          <AddIcon />
+        </Fab>
+      </Card>
+    </form>
+  );
+};
+
+export default StepTwo;
