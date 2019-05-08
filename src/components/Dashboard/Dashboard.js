35779156d50a8a422bb3f7acc3e6448528d3e05c
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-08 12:56:49 -0400

    Info that is no longer used in Dashboard removed.

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,10 +3,9 @@ import { connect } from 'react-redux';
 
 import { fetchUser } from '../../actions';
 import Meds from '../Meds/Meds';
-import Diary from '../Diary/Diary';
 
 const Dashboard = ({ fetchUser, user }) => {
-  const { username, premium, email, phone, first_name, last_name } = user;
+  const { username } = user;
   const userID = localStorage.getItem('userID');
   useEffect(() => {
     if (!user.auth_id) {
@@ -16,23 +15,7 @@ const Dashboard = ({ fetchUser, user }) => {
 
   return (
     <div className='DashboardPage'>
-      <h2>User Dashboard</h2>
-      {username ? (
-        <div className='user'>
-          <h2>
-            Welcome <em>{username}</em> !
-          </h2>
-          <h2>Account Information:</h2>
-          <p>Username: {username}</p>
-          <p>Account type: {premium ? 'premium' : 'trial'}</p>
-          <p>Email address: {email}</p>
-          <p>Phone number: {phone}</p>
-          <p>First Name: {first_name}</p>
-          <p>Last Name: {last_name}</p>
-          <Meds user_id={userID} />
-          <Diary user_id={userID} />
-        </div>
-      ) : null}
+      {username ? <Meds user_id={userID} /> : null}
     </div>
   );
 };
