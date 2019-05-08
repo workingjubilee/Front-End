8e1996ad9d7e04f66e4365d72f6aa299d35962a0
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-08 13:36:53 -0400

    Refactored Meds.js into class component. Will check if user has meds, and if they don't, displays a message.

--- src/components/Meds/Meds.js
+++ src/components/Meds/Meds.js
@@ -1,32 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { connect } from 'react-redux';
 import { fetchMeds } from '../../actions';
 import Med from '../Meds/Med';
 import { withStyles } from '@material-ui/core/styles';
 
-const Meds = ({ fetchMeds, user_id, meds, fetchingMeds, classes }) => {
-  useEffect(() => {
-    if (meds.length === 0) {
-      fetchMeds(user_id);
+class Meds extends Component {
+  state = {
+    noMeds: false
+  };
+  componentDidMount() {
+    if (this.props.meds.length === 0) {
+      this.props.fetchMeds(this.props.user_id);
     }
-  }, [user_id, fetchMeds]);
-
-  if (fetchingMeds) {
-    return <h2>Loading Meds...</h2>;
-  } else {
-    return (
-      <Card className={classes.card}>
-        <h2>Your scheduled medications for today</h2>
-        <div className={classes.meds}>
-          {meds.map(med => (
-            <Med key={med.id} med={med} />
-          ))}
-        </div>
-      </Card>
-    );
   }
-};
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.fetchingMeds &&
+      !this.props.fetchingMeds &&
+      !this.props.error &&
+      this.props.meds.length === 0
+    ) {
+      this.setState({ noMeds: true });
+    }
+  }
+
+  render() {
+    const { fetchingMeds, classes, meds } = this.props;
+    if (fetchingMeds) {
+      return (
+        <div className={classes.loading}>
+          <h2>Loading Meds...</h2>;
+          <CircularProgress
+            className={classes.progress}
+            color='primary'
+            disableShrink
+          />
+        </div>
+      );
+    } else if (this.state.noMeds) {
+      return <h1>You do not have any meds saved yet.</h1>;
+    } else {
+      return (
+        <Card className={classes.card}>
+          <h2>Your scheduled medications for today</h2>
+          <div className={classes.meds}>
+            {meds.map(med => (
+              <Med key={med.id} med={med} />
+            ))}
+          </div>
+        </Card>
+      );
+    }
+  }
+}
 
 const styles = theme => ({
   progress: {
@@ -39,6 +68,10 @@ const styles = theme => ({
   meds: {
     display: 'flex',
     flexWrap: 'wrap'
+  },
+  loading: {
+    margin: '0 auto',
+    width: 500
   }
 });
 
