c307fdbd82a064535fd6bf936bbee2b4ca8f8cec
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-08 12:57:18 -0400

    Meds is also a card.

--- src/components/Meds/Meds.js
+++ src/components/Meds/Meds.js
@@ -1,29 +1,44 @@
 import React, { useEffect } from 'react';
+import Card from '@material-ui/core/Card';
 import { connect } from 'react-redux';
-
 import { fetchMeds } from '../../actions';
 import Med from '../Meds/Med';
+import { withStyles } from '@material-ui/core/styles';
 
-const Meds = ({ fetchMeds, user_id, meds }) => {
+const Meds = ({ fetchMeds, user_id, meds, fetchingMeds, classes }) => {
   useEffect(() => {
     fetchMeds(user_id);
   }, [user_id, fetchMeds]);
 
-  return (
-    <div className='meds'>
-      {meds.length > 0 ? (
-        <div>
-          <h2>Meds:</h2>
+  if (fetchingMeds) {
+    return <h2>Loading Meds...</h2>;
+  } else {
+    return (
+      <Card className={classes.card}>
+        <h2>Your scheduled medications for today</h2>
+        <div className={classes.meds}>
           {meds.map(med => (
             <Med key={med.id} med={med} />
           ))}
         </div>
-      ) : (
-        <h2>Loading Meds...</h2>
-      )}
-    </div>
-  );
+      </Card>
+    );
+  }
 };
+
+const styles = theme => ({
+  progress: {
+    margin: theme.spacing.unit * 2
+  },
+  card: {
+    maxWidth: 1100,
+    margin: '0 auto'
+  },
+  meds: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  }
+});
 
 const mapStateToProps = state => ({
   meds: state.meds,
@@ -31,7 +46,9 @@ const mapStateToProps = state => ({
   error: state.error
 });
 
+const StyledMeds = withStyles(styles)(Meds);
+
 export default connect(
   mapStateToProps,
   { fetchMeds }
-)(Meds);
+)(StyledMeds);
