bb28bc9fc71d06cb52abd66a94d979aff1f6ecc3
Jacob Angulo <jacobangulo@hotmail.com>
2019-05-08 16:04:19 -0700

    built out and wired up stepThree component where pill data and dosage data are displayed before user adds pill

---
+++ src/components/AddPill/AddPillSteps/StepThree.js
@@ -0,0 +1,93 @@
+// import React from 'react';
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { colors } from '../../data/colors';
+import { shapes } from '../../data/shapes';
+
+const SimpleCard = props => {
+  return (
+    <Card>
+      <Card>
+        <CardContent>
+          <div style={{ display: 'flex' }}>
+            <Typography component='p' variant='title'>
+              Pill Name
+            </Typography>
+            <Typography component='p' variant='headline'>
+              {props.name.value}
+            </Typography>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <Typography component='p' variant='title'>
+              Imprint
+            </Typography>
+            <Typography component='p' variant='headline'>
+              {props.imprint.value}
+            </Typography>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <Typography component='p' variant='title'>
+              Color
+            </Typography>
+            <Typography component='p' variant='headline'>
+              {colors[props.color].name}
+            </Typography>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <Typography component='p' variant='title'>
+              Shape
+            </Typography>
+            <Typography component='p' variant='headline'>
+              {shapes[props.shape].name}
+            </Typography>
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardContent>
+          <div style={{ display: 'flex' }}>
+            <Typography component='p' variant='title'>
+              Capsules Per Dose
+            </Typography>
+            <Typography component='p' variant='headline'>
+              {props.capsulesPerDose}
+            </Typography>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <Typography component='p' variant='title'>
+              Length of Dosage
+            </Typography>
+            <Typography component='p' variant='headline'>
+              {props.lengthOfDosage}
+            </Typography>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <Typography component='p' variant='title'>
+              Dosage Frequency
+            </Typography>
+            <Typography component='p' variant='headline'>
+              {props.dosageFrequency}
+            </Typography>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <Typography component='p' variant='title'>
+              Dosage Instruction
+            </Typography>
+            <Typography component='p' variant='headline'>
+              {props.dosageInstruction}
+            </Typography>
+          </div>
+        </CardContent>
+      </Card>
+      <CardActions>
+        <Button>Add Pill</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default SimpleCard;
