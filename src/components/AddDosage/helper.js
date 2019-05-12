8269465bac186541757d1f9f4f036947847b261b
Jacob Angulo <jacobangulo@hotmail.com>
2019-05-11 17:46:01 -0700

    versatile helper func with the ability to take various info about reminder times and duration and spit out reminders within set paramaters

--- src/components/AddDosage/helper.js
+++ src/components/AddDosage/helper.js
@@ -7,8 +7,7 @@ export const makeReminders = (
   frequency,
   date,
   weekday,
-  time,
-  instruction
+  time
 ) => {
   startDate = moment(startDate).format('LLLL');
   endDate = moment(endDate).format('LLLL');
@@ -27,10 +26,10 @@ export const makeReminders = (
 
   let lastRem = new Date(startDate).getTime();
 
+  let reminders = [];
+
   for (let i = 0; i < doses; i++) {
     const setRemTime = rem => {
-      console.log('makes it here');
-      console.log(rem);
       rem[4] = time[i];
       rem[4] = rem[4].split('');
 
@@ -65,97 +64,41 @@ export const makeReminders = (
         newRem = newRem.split(' ');
         newRem = setRemTime(newRem);
         newRem = newRem.join(' ');
-        console.log(newRem);
-        console.log(new Date(newRem).getTime());
+        newRem = new Date(newRem).getTime();
+        reminders.push(newRem);
         lastRem += 86400000;
       } else if (frequency === 'weekly') {
         let newRem = moment(lastRem).format('LLLL');
         newRem = newRem.split(' ');
-        console.log(newRem)
         if (newRem[2] === weekday[i]) {
           newRem = setRemTime(newRem);
           newRem = newRem.join(' ');
-          console.log(newRem);
-          console.log(new Date(newRem).getTime());
         }
+        newRem = new Date(newRem).getTime();
+        reminders.push(newRem);
         lastRem += 86400000;
       } else if (frequency === 'monthly') {
         let newRem = moment(lastRem).format('LLLL');
         newRem = newRem.split(' ');
-        // console.log(newRem)
-        console.log(date[i])
-        if (parseInt(newRem[2][0]) === date[i]) {
-          newRem = setRemTime(newRem);
-          newRem = newRem.join(' ');
-          console.log(newRem);
-          console.log(new Date(newRem).getTime());
+        if (newRem[2].length === 3) {
+          if (parseInt(newRem[2][0] + newRem[2][1]) === date[i]) {
+            newRem = setRemTime(newRem);
+            newRem = newRem.join(' ');
+            newRem = new Date(newRem).getTime();
+            reminders.push(newRem);
+          }
+        } else if (newRem[2].length === 2) {
+          if (parseInt(newRem[2][0]) === date[i]) {
+            newRem = setRemTime(newRem);
+            newRem = newRem.join(' ');
+            newRem = new Date(newRem).getTime();
+            reminders.push(newRem);
+          }
         }
         lastRem += 86400000;
       }
     }
     lastRem = new Date(startDate).getTime();
   }
+  return reminders;
 };
-//   // console.log(moment(startDate).format('LLLL'));
-//   // console.log(instruction);
-//   let offset = new Date().getTimezoneOffset() / 60;
-//   startDate = startDate.split('');
-//   endDate = endDate.split('');
-//   let startDateHour = offset.toString();
-//   let endDateHour = (offset - 1).toString();
-//   if (startDateHour.length === 1) {
-//     startDateHour = `0${startDateHour}`;
-//   } else {
-//     startDateHour = startDateHour.toString();
-//   }
-//   if (endDateHour.length === 1) {
-//     endDateHour = `0${endDateHour}`;
-//   } else {
-//     endDateHour = endDateHour.toString();
-//   }
-//   startDate[11] = startDateHour[0];
-//   startDate[12] = startDateHour[1];
-//   startDate[14] = '0';
-//   startDate[15] = '0';
-//   startDate[17] = '0';
-//   startDate[18] = '0';
-//   startDate[20] = '0';
-//   startDate[21] = '0';
-//   startDate[22] = '0';
-//   endDate[11] = endDateHour[0];
-//   endDate[12] = endDateHour[1];
-//   endDate[14] = '5';
-//   endDate[15] = '9';
-//   endDate[17] = '5';
-//   endDate[18] = '9';
-//   endDate[20] = '9';
-//   endDate[21] = '9';
-//   endDate[22] = '9';
-//   let timeOnDeck = new Date(startDate.join('')).getTime();
-//   let reminders = [];
-//   while (timeOnDeck <= new Date(endDate.join('')).getTime()) {
-//     if (frequency === 'daily') {
-//       reminders.push(timeOnDeck);
-//       // console.log(day);
-//       // console.log(new Date(moment(timeOnDeck).format('LLLL')).getTime());
-//       timeOnDeck += 86400000;
-//     } else if (frequency === 'weekly') {
-//       let day = moment(timeOnDeck).format('LLLL');
-//       day = day.toString();
-//       day = `${day}`;
-//       day = day.split(' ');
-//       if (day[0] === weekday) {
-//         console.log(day);
-//       }
-//       reminders.push(timeOnDeck);
-//       timeOnDeck += 86400000;
-//       // timeOnDeck += 604800000;
-//     } else if (frequency === 'monthly') {
-//       // console.log(moment.format(timeOnDeck));
-//       reminders.push(timeOnDeck);
-//       timeOnDeck += 2419200000;
-//     }
-//   }
-//   console.log(reminders);
-//   return reminders;
-// };
