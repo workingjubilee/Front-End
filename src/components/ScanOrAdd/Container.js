553a4656cdfd23d0986e1bce6fe9dc9fbd77a9ab
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-14 13:44:20 -0400

    Refactoring to no longer use tab navigator

--- src/components/ScanOrAdd/Container.js
+++ src/components/ScanOrAdd/Container.js
@@ -2,11 +2,7 @@ import React, { useReducer } from 'react';
 import Scan from './Scan/Scan.js'; // Prioritizing the Scan component
 import scanReducer, { init } from './scanReducer.js';
 import Paper from '@material-ui/core/Paper';
-import Tabber from './Tabber.js';
-import Spinner from 'components/Spinner/Spinner.js'
-
-const SearchPill = React.lazy(() =>
- import('./SearchPill/SearchPill')); // Lazy-loading only imports if it needs to
+import SearchPill from './SearchPill/SearchPill';
 
 export default function ScanOrAdd({ location, history }) {
   const [state, dispatch] = useReducer(scanReducer, init(location));
@@ -15,13 +11,8 @@ export default function ScanOrAdd({ location, history }) {
 
   return (
     <Paper square>
-      <Tabber state={state} dispatch={dispatch} />
-      <React.Suspense fallback={<Spinner />}>
-        { state.tab === 0
-          ? <Scan state={state} dispatch={dispatch} />
-          : <SearchPill state={state} dispatch={dispatch} />
-        }
-      </React.Suspense>
+      <Scan state={state} dispatch={dispatch} />
+      <SearchPill state={state} dispatch={dispatch} />
     </Paper>
   );
-};
+}
