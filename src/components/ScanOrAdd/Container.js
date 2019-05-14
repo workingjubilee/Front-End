1e095e0fa0163c03673b808546152f4860d68dbb
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-14 14:52:38 -0400

    Logic for handling both modals added, as well as handling adding a pill. Pill info is set in state by the pill info modal

--- src/components/ScanOrAdd/Container.js
+++ src/components/ScanOrAdd/Container.js
@@ -1,11 +1,60 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
+import { connect } from 'react-redux';
+import { addMed } from '../../actions';
 import Scan from './Scan/Scan.js'; // Prioritizing the Scan component
 import scanReducer, { init } from './scanReducer.js';
 import Paper from '@material-ui/core/Paper';
 import SearchPill from './SearchPill/SearchPill';
+import PillInfoModal from '../Modals/PillInfoModal';
+import AddPillModal from '../Modals/AddPillModal';
 
-export default function ScanOrAdd({ location, history }) {
+function ScanOrAdd({ location, history, addMed }) {
   const [state, dispatch] = useReducer(scanReducer, init(location));
+  const [pill, setPill] = useState({});
+  const [open, setOpen] = useState(false);
+  const [confirmOpen, setConfirmOpen] = useState(false);
+  console.log(pill);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleConfirm = pillInfo => {
+    handleClose();
+    setPill(pillInfo);
+    handleConfirmOpen();
+  };
+
+  const handleConfirmOpen = () => {
+    setConfirmOpen(true);
+  };
+
+  const handleConfirmClose = () => {
+    setConfirmOpen(false);
+  };
+
+  const handleAddPill = () => {
+    addMed(pill)
+      .then(() => {
+        history.push('/pills');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const handleAddPillReminders = () => {
+    addMed(pill)
+      .then(() => {
+        history.push('/adddosage');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   console.log(state); // purely for debugging
 
@@ -13,7 +62,30 @@ export default function ScanOrAdd({ location, history }) {
     <Paper square>
       <Scan state={state} dispatch={dispatch} />
       <SearchPill state={state} dispatch={dispatch} />
-      <p>Add Manually</p>
+      <p onClick={handleOpen}>Add Manually</p>
+      <PillInfoModal
+        open={open}
+        handleConfirm={handleConfirm}
+        handleClose={handleClose}
+        setPill={setPill}
+      />
+      <AddPillModal
+        open={confirmOpen}
+        pill={pill}
+        handleClose={handleConfirmClose}
+        handleAddPill={handleAddPill}
+        handleAddPillReminders={handleAddPillReminders}
+      />
     </Paper>
   );
 }
+
+const mapStateToProps = state => ({
+  addingMed: state.addingMed,
+  error: state.error
+});
+
+export default connect(
+  mapStateToProps,
+  { addMed }
+)(ScanOrAdd);
