645e8d3a153d7f79cf44e2724f2f6c57ed79645e
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-05-14 19:10:38 -0400

    Demonstating MUI customization.

--- src/components/Diary/DiaryEntries.js
+++ src/components/Diary/DiaryEntries.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CardActions from '@material-ui/core/CardActions';
+import Card from '@material-ui/core/Card';
+import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 import DiaryEntry from './DiaryEntry';
 import DiaryEntryModal from './DiaryEntryModal';
 
-const DiaryEntries = ({ diary, diaryFocus, meds }) => {
+const ExpansionPanelSummary = withStyles({
+  content: {
+    justifyContent: 'space-around'
+  }
+})(props => <MuiExpansionPanelSummary {...props} />);
+
+ExpansionPanelSummary.muiName = 'ExpansionPanelSummary';
+
+const styles = {
+  newEntryPanel: {
+    border: '1px dashed red'
+  }
+};
+
+const DiaryEntries = ({ classes, diary, diaryFocus, meds }) => {
   const filteredDiary = diary.filter(diaryEntry => {
     return diaryEntry.med_id === diaryFocus;
   });
@@ -19,28 +36,21 @@ const DiaryEntries = ({ diary, diaryFocus, meds }) => {
 
   return (
     <div className='diaryEntries'>
-      {meds.length === 0 ? (
-        <div />
-      ) : !diaryFocus ? (
-        <h2>Choose a medication to view diary entries...</h2>
+      <ExpansionPanelSummary className={classes.newEntryPanel}>
+        <DiaryEntryModal
+          className={classes.newEntryPanel}
+          newEntry={true}
+          medName={medName}
+          med_id={diaryFocus}
+        />
+      </ExpansionPanelSummary>
+
+      {filteredDiary.length === 0 ? (
+        <p>You have no diary entries for {medName}.</p>
       ) : (
-        <div>
-          <CardActions className='diaryEntriesHeader'>
-            <h2>Entries</h2>
-            <DiaryEntryModal
-              newEntry={true}
-              medName={medName}
-              med_id={diaryFocus}
-            />
-          </CardActions>
-          {filteredDiary.length === 0 ? (
-            <p>You have no diary entries for {medName}.</p>
-          ) : (
-            filteredDiary.map(diaryEntry => (
-              <DiaryEntry key={diaryEntry.id} diaryEntry={diaryEntry} />
-            ))
-          )}
-        </div>
+        filteredDiary.map(diaryEntry => (
+          <DiaryEntry key={diaryEntry.id} diaryEntry={diaryEntry} />
+        ))
       )}
     </div>
   );
@@ -54,4 +64,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   null
-)(DiaryEntries);
+)(withStyles(styles)(DiaryEntries));
