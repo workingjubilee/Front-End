ac35fe8bfdd39aa02cdc563e493a998fcddcd578
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-05-15 15:12:30 -0400

    Tracked down and eliminiated minor bug.

--- src/components/Diary/DiaryEntryPanel.js
+++ src/components/Diary/DiaryEntryPanel.js
@@ -13,10 +13,7 @@ const ExpansionPanelSummary = withStyles({
     backgroundColor: 'rgba(0,0,0,.03)',
     borderBottom: '1px solid rgba(0,0,0,.125)',
     marginBottom: -1,
-    minHeight: 56,
-    '&$expanded': {
-      minHeight: 56
-    }
+    minHeight: 56
   },
   content: {
     '&> :last-child': {
