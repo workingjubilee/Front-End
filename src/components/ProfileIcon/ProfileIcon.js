4ebdc54862d3fc7460a65c6a267cbe291c6d56b9
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-16 16:05:24 -0400

    Refactored ProfileIcon into class component. Will check for presence of user, meds, and rems, and fetch them if they are not present (if user is logged in).

--- src/components/ProfileIcon/ProfileIcon.js
+++ src/components/ProfileIcon/ProfileIcon.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Card from '@material-ui/core/Card';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
+import { fetchUser, fetchMeds, fetchRems } from 'actions';
+
+class ProfileIcon extends Component {
+  componentDidMount() {
+    const { user, fetchUser, fetchMeds, fetchRems, meds, rems } = this.props;
+    const userID = user.id ? user.id : localStorage.getItem('userID');
+
+    if (!user.username && userID) {
+      fetchUser(userID);
+    }
+    if (meds.length === 0 && userID) {
+      fetchMeds(userID);
+    }
+    if (rems.length === 0 && userID) {
+      fetchRems(userID);
+    }
+  }
+  render() {
+    const { classes, user, filteredRems } = this.props;
+    const { username } = user;
+    return (
+      <Card className={classes.paper}>
+        <div className={classes.alignLeft}>
+          <Typography className={classes.text}>Hello {username} </Typography>
+          <Card>
+            <Typography className={classes.lowerText}>
+              {filteredRems.length} pills today
+            </Typography>
+          </Card>
+        </div>
+        <Avatar className={classes.avatar} alt={user.username} />
+      </Card>
+    );
+  }
+}
 
 const styles = theme => ({
   paper: {
@@ -29,31 +64,16 @@ const styles = theme => ({
   }
 });
 
-const ProfileIcon = ({ classes, user, filteredRems }) => {
-  const { username } = user;
-  return (
-    <Card className={classes.paper}>
-      <div className={classes.alignLeft}>
-        <Typography className={classes.text}>Hello {username} </Typography>
-        <Card>
-          <Typography className={classes.lowerText}>
-            {filteredRems.length} pills today
-          </Typography>
-        </Card>
-      </div>
-      <Avatar className={classes.avatar} alt={user.username} />
-    </Card>
-  );
-};
-
 const mapStateToProps = state => ({
   user: state.userReducer.user,
-  filteredRems: state.medsReducer.filteredRems
+  meds: state.medsReducer.meds,
+  rems: state.remsReducer.rems,
+  filteredRems: state.remsReducer.filteredRems
 });
 
 const StyledProfileIcon = withStyles(styles)(ProfileIcon);
 
 export default connect(
   mapStateToProps,
-  null
+  { fetchUser, fetchMeds, fetchRems }
 )(StyledProfileIcon);
