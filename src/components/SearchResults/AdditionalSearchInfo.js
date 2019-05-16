7414ff5d1a4387975f2c3969b8b71fd6fb29f641
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-05-16 10:22:12 -0500

    fixes import error

--- src/components/SearchResults/AdditionalSearchInfo.js
+++ src/components/SearchResults/AdditionalSearchInfo.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-const AdditionalSearchInfo = () => {
+const styles = {
+  card: {
+    boxShadow: 'none'
+  }
+};
+
+const AdditionalSearchInfo = ({ classes }) => {
   return (
-    <Card>
+    <Card className={classes.card}>
       <Typography variant='h5'>Can't find what you're looking for?</Typography>
     </Card>
   );
 };
 
-export default AdditionalSearchInfo;
+export default withStyles(styles)(AdditionalSearchInfo);
