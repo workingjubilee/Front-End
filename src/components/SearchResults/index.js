d25c30299f4a07ca3ee77d10e33da3c3c6b60230
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-05-16 15:17:48 -0500

    modifying search

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { connect } from 'react-redux';
 import AdditionalSearchInfo from './AdditionalSearchInfo';
 import SearchResult from './SearchResult';
@@ -7,6 +7,19 @@ const SearchResults = ({ searchResults }) => {
   const style = {
     display: 'flex'
   };
+
+  const pillTemplate = {
+    user_id: localStorage.getItem('userID'),
+    med_name: '',
+    med_color: '',
+    med_shape: '',
+    med_strength: 0,
+    med_strength_unit: '',
+    med_dose: 0,
+    med_dose_unit: ''
+  };
+
+  const [pill, setPill] = useState(pillTemplate);
 
   return (
     <div style={style}>
