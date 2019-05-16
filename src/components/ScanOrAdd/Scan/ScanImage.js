845d62437ff85a6e9912e9090202e3148d814ac9
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-15 22:20:22 -0400

    Commented out previous axios call, invoked action creator and passed in imgData.

--- src/components/ScanOrAdd/Scan/ScanImage.js
+++ src/components/ScanOrAdd/Scan/ScanImage.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import ImageCapture from './ImageCapture.js';
-import axios from 'axios';
+// import axios from 'axios';
 import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+import { uploadImage } from 'actions';
 
-export default function ScanImage({ state, dispatch }) {
+function ScanImage({ state, dispatch, uploadImage, history }) {
   const [photo, setPhoto] = useState(null);
   const [displayCamera, toggleDisplayCamera] = useState(false);
 
@@ -15,14 +17,25 @@ export default function ScanImage({ state, dispatch }) {
       console.log('Need a photo!');
       return;
     }
-    const photoEndpoint = `${process.env.REACT_APP_BACKEND}/api/upload`;
+    // const photoEndpoint = `${process.env.REACT_APP_BACKEND}/api/upload`;
     const imgData = new FormData();
     imgData.append('image', photo, photo.filename);
 
-    const result = await axios
-      .post(photoEndpoint, imgData)
-      .catch(err => console.error(err));
-    dispatch({ type: 'analysisResults', payload: result.data });
+    uploadImage(imgData)
+      .then(() => {
+        history.push('/searchresults');
+      })
+      .catch(err => {
+        console.error(err);
+        alert(`Image upload failed. ${err}.`);
+      });
+    // Not sure if you still need this so I'll leave it
+    // try {
+    //   const result = await axios.post(photoEndpoint, imgData);
+    //   dispatch({ type: 'analysisResults', payload: result.data });
+    // } catch (error) {
+    //   console.error(error);
+    // }
   };
 
   return (
@@ -57,3 +70,8 @@ export default function ScanImage({ state, dispatch }) {
     </>
   );
 }
+
+export default connect(
+  null,
+  { uploadImage }
+)(ScanImage);
