1a2ca7df1bfd4a72c68f302362eac399628f879d
workingjubilee <workingjubilee@gmail.com>
2019-05-17 08:26:50 -0700

    Makes search effing work

--- src/components/ScanOrAdd/SearchPill/SearchPill.js
+++ src/components/ScanOrAdd/SearchPill/SearchPill.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { connect } from 'react-redux';
 import { useInput } from 'utilities/useInput';
 import { valid_shapes as shapes } from 'data/rxdata.json';
@@ -6,39 +7,39 @@ import { valid_colors as colors } from 'data/rxdata.json';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Dropdown from './Dropdown';
+import parseMedStrengths from 'utilities/parseMedStrengths';
 
 // import AddPillButton from '../Scan/SearchResults/AddPillButton';
 
-const SearchPill = props => {
+const SearchPill = ({dispatch, ...rest}) => {
   const name = useInput();
   const imprint = useInput();
   const [color, setColor] = useState('');
   const [shape, setShape] = useState('');
 
-  // useEffect(() => {}, [color, shape]);
-  const handleColorChange = e => {
-    setColor(e.target.value);
-  };
-  const handleShapeChange = e => {
-    setShape(e.target.value);
-  };
-  const handleAddPill = e => {
-    e.preventDefault();
-  };
-  const handleSubmit = e => {
-    e.preventDefault();
+  const textEndpoint = `${process.env.REACT_APP_DATA_SCIENCE}/rxdata`;
 
-    props.setSearchResults([
-      {
-        pillName: name.value,
-        imageLink:
-          'https://www.drugs.com/images/pills/mmx/t110118f/tizanidine-hydrochloride.jpg'
-      }
-    ]);
+  // useEffect(() => {}, [color, shape]);
+  const search = async (e) => {
+    e.preventDefault();
+    const query = {
+      "pill_name": name
+      imprint,
+      shape,
+      color
+    };
+    console.log(query);
+    try {
+      const results = await axios.post(textEndpoint, query);
+      const parsedResults = parseMedStrengths(results.data);
+      dispatch({ 'type': 'analysisResults', payload: parsedResults })
+    } catch(error) {
+      console.error(error);
+    };
     // Search for pill
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={search}>
       <h4>Option 2 - Identify by direct input</h4>
       <TextField
         label='pill name'
@@ -57,7 +58,7 @@ const SearchPill = props => {
       <Dropdown itemName="color" itemList={colors} item={color} setItem={setColor} />
       <Dropdown itemName="shape" itemList={shapes} item={shape} setItem={setShape} />
 
-      <Button onClick={handleAddPill} variant='contained'>
+      <Button onClick={search} variant='contained'>
         Identify Pill
       </Button>
     </form>
