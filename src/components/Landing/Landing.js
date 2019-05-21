38bfb48a42f4e340fa64a8782d322735a90f3c7a
Jacob Angulo <jacobangulo@hotmail.com>
2019-05-21 15:58:22 -0700

    built out jsx for core features section of the landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,75 +1,35 @@
 import React from 'react';
 import Auth from 'Auth';
+import ArrowIcon from '@material-ui/icons/CompareArrows';
+import DiaryIcon from '@material-ui/icons/QuestionAnswer';
+import ScanIcon from '@material-ui/icons/CenterFocusStrong';
+import PillIcon from '@material-ui/icons/Link';
+// import '../../scss/Landing.scss';
 
 const Landing = () => {
   return (
-    <div style={{ boxSizing: 'border-box' }}>
+    <div className='landing'>
       {/* header */}
-      <header
-        style={{
-          display: 'flex',
-          background:
-            'radial-gradient(farthest-side at 50% 0%, #2D8BEC, #252C35 90%)'
-        }}
-      >
-        <div
-          style={{
-            margin: '150px 50px 50px 100px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'flex-start',
-            width: '50%'
-          }}
-        >
-          <h1
-            style={{
-              fontSize: '60px',
-              fontWeight: '000',
-              color: '#FFFFFF'
-            }}
-          >
+      <header className='header'>
+        <div className='product-data'>
+          <h1>
             Welcome to <br />
             RxID Pill Identifier
           </h1>
-          <hr
-            style={{
-              width: '300px',
-              marginLeft: '-150px',
-              border: '2.5px solid #40AB48',
-              justifyContent: 'flex-start'
-            }}
-          />
-          <p style={{ color: '#74869B' }}>
+          <hr />
+          <p>
             Identify your medications on their appearance and log each
             medication and your experience with them. Schedule your medications
             and get notifications minutes before due time with image and dosage
             amount to be taken.
           </p>
         </div>
-        <div
-          style={{
-            width: '50%',
-            display: 'flex',
-            justifyContent: 'space-evenly',
-            alignItems: 'center',
-            margin: '150px 100px 50px 50px',
-            lineHeight: '100%'
-          }}
-        >
+        <div className='buttons'>
           <button
+            className='login'
             onClick={event => {
               event.preventDefault();
               Auth.lock.show();
-            }}
-            style={{
-              color: '#E8EBEF',
-              width: '180px',
-              height: '60px',
-              fontSize: '20px',
-              background: 'none',
-              border: '1px solid #979797',
-              borderRadius: '5px',
-              cursor: 'pointer'
             }}
           >
             Login
@@ -79,24 +39,15 @@ const Landing = () => {
               event.preventDefault();
               Auth.lock.show();
             }}
-            style={{
-              fontSize: '20px',
-              color: '#E8EBEF',
-              width: '180px',
-              height: '60px',
-              background: '#2D90F5',
-              border: '1px solid #2D90F5',
-              borderRadius: '5px',
-              cursor: 'pointer'
-            }}
+            className='join'
           >
             Join us
           </button>
         </div>
       </header>
       {/* core features */}
-      <section>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <section className='core-features'>
+        <div className='text-content'>
           <h1>
             Our Core
             <br />
@@ -104,7 +55,38 @@ const Landing = () => {
           </h1>
           <p>Check out our amazing features</p>
         </div>
+        <div className='tiles'>
+          <div className='tiles-column'>
+            <div className='scan-tile'>
+              <ScanIcon className='scan-icon' />
+              <p>Scan & Identify Pills</p>
+            </div>
+            <div className='tile'>
+              <PillIcon className='icon' />
+              <p>Medication List</p>
+            </div>
+          </div>
+          <div className='tiles-column'>
+            <div className='tile'>
+              <ArrowIcon className='icon' />
+              <p>Schedule Pills</p>
+            </div>
+            <div className='tile'>
+              <DiaryIcon className='icon' />
+              <p>Medication Diary</p>
+            </div>
+          </div>
+        </div>
       </section>
+      {/* testimonials */}
+      <section className='testimonials'>
+        <div className='text-content'>
+          <h1>Testimonials</h1>
+          <p>What our users said about our product</p>
+        </div>
+      </section>
+      {/* team */}
+      {/* footer */}
     </div>
   );
 };
