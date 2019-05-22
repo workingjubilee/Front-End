21cb6a099862f9a846193412688a2bd3d50a448d
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-05-22 19:42:28 -0400

    Diary styling.

--- src/components/Diary/DiaryEmojiGrid.js
+++ src/components/Diary/DiaryEmojiGrid.js
@@ -12,15 +12,25 @@ const styles = () => ({
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
-    overflow: 'hidden',
-    border: '1px dashed red'
+    overflow: 'hidden'
   },
   gridList: {
     maxWidth: 500,
     height: 165
   },
-  tile: { background: 'white,', color: 'black' },
-  selectedTile: { background: 'teal', color: 'white' },
+  tile: {
+    fontSize: '12px',
+    padding: '0 1px',
+    background: 'white,',
+    color: 'black'
+  },
+  selectedTile: {
+    fontSize: '12px',
+    padding: '0 1px',
+    background: '#0A3E73',
+    color: 'white',
+    borderRadius: '4px'
+  },
   image: {
     maxWidth: '100%',
     maxHeight: 'auto'
@@ -35,47 +45,51 @@ function DiaryEmojiGrid({ classes, diaryEmoji, updateDiaryEmoji }) {
     <div className={classes.root}>
       <GridList cellHeight={80} className={classes.gridList} cols={6}>
         {diaryEmojiTiles.map(tile => (
-          <div
-            onClick={() => updateDiaryEmoji(tile.value)}
-            className={
-              tile.value === diaryEmoji ? classes.selectedTile : classes.tile
-            }
-            key={tile.value}
-          >
-            <GridListTile cols={tile.cols || 1}>
-              <div>
-                <img
-                  className={classes.image}
-                  src={tile.img}
-                  alt={tile.title}
-                />
-              </div>
-            </GridListTile>
+          <div className={classes.fullTile}>
             <div
+              onClick={() => updateDiaryEmoji(tile.value)}
               className={
                 tile.value === diaryEmoji ? classes.selectedTile : classes.tile
               }
+              key={tile.value}
             >
-              <Typography
-                className={classNames(
-                  classes.label,
+              <GridListTile cols={tile.cols || 1}>
+                <div>
+                  <img
+                    className={classes.image}
+                    src={tile.img}
+                    alt={tile.title}
+                  />
+                </div>
+              </GridListTile>
+              <div
+                className={
                   tile.value === diaryEmoji
                     ? classes.selectedTile
                     : classes.tile
-                )}
+                }
               >
-                {tile.text}
-              </Typography>
-              <Typography
-                className={classNames(
-                  classes.label,
-                  tile.value === diaryEmoji
-                    ? classes.selectedTile
-                    : classes.tile
-                )}
-              >
-                {tile.number}
-              </Typography>
+                <Typography
+                  className={classNames(
+                    classes.label,
+                    tile.value === diaryEmoji
+                      ? classes.selectedTile
+                      : classes.tile
+                  )}
+                >
+                  {tile.text}
+                </Typography>
+                <Typography
+                  className={classNames(
+                    classes.label,
+                    tile.value === diaryEmoji
+                      ? classes.selectedTile
+                      : classes.tile
+                  )}
+                >
+                  {tile.number}
+                </Typography>
+              </div>
             </div>
           </div>
         ))}
