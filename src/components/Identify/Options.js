3e4545fdd3a8245b6a24483e7f72ddf283452047
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-05-22 14:10:59 -0400

    added section for the header image

--- src/components/Identify/Options.js
+++ src/components/Identify/Options.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ByImage from './ByImage'; // Prioritizing the Scan component
 import ByForm from './ByForm';
 import OrAdd from './OrAdd.js';
+import Button from '@material-ui/core/Button';
 
 const IdentifyOptions = ({ addPill, setData, ...props }) => {
   return (
     <section className='scan-container'>
-      <h2>Identify your Pill before scheduling</h2>
-      <ByImage setData={setData} />
-      <ByForm setData={setData} />
-      <OrAdd addPill={addPill} />
+      <header className='header-logo'>
+        <h1>
+          RxID <br /> Pill Identifier
+        </h1>
+        <h4>
+          Identify your medications based on their appearance and log each
+          <br />
+          medication and your experiences with them.
+        </h4>
+        <Button variant='contained'>Learn more</Button>
+      </header>
+      <section className='options-container'>
+        <h2>Identify your Pill before scheduling</h2>
+        <ByImage setData={setData} />
+        <ByForm setData={setData} />
+        <OrAdd addPill={addPill} />
+      </section>
     </section>
   );
 };
