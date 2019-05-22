6127a080698c6f5af526b1a846f079613a8b519c
workingjubilee <workingjubilee@gmail.com>
2019-05-22 06:06:06 -0700

    Factors out piecse to turn Identify into a mini-router

--- src/components/Identify/index.js
+++ src/components/Identify/index.js
@@ -3,36 +3,21 @@ import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addMed } from 'actions';
 
-import ByImage from './ByImage'; // Prioritizing the Scan component
-import ByForm from './ByForm';
-import OrAdd from './OrAdd.js';
 import Spinner from 'components/Spinner/Spinner.js';
 
+const IdentifyOptions = lazy(() => import('./Options'));
 const SearchResults = lazy(() => import('./Results'));
 
-function ScanOrAdd({ location, history, addMed, ...props }) {
+function Identify({ match, location, history, addMed, ...props }) {
   const [data, setData] = useState();
 
-  const handleAddPill = pillInfo => {
+  const addPill = (pillInfo, destination='pills') => {
     addMed({
       ...pillInfo,
       med_add_date: new Date().getTime()
     })
       .then(() => {
-        history.push('/pills');
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
-  const handleAddPillReminders = pillInfo => {
-    addMed({
-      ...pillInfo,
-      med_add_date: new Date().getTime()
-    })
-      .then(() => {
-        history.push('/adddosage');
+        history.push(`/${destination}`);
       })
       .catch(error => {
         console.log(error);
@@ -40,30 +25,25 @@ function ScanOrAdd({ location, history, addMed, ...props }) {
   };
 
   return (
-      <Suspense fallback={<Spinner />}>
-      {data ? ( 
-        <SearchResults
-          searchResults={data}
-          handleAddPill={handleAddPill}
-          handleAddPillReminders={handleAddPillReminders}
-          history={history}
-        />
-      ) : (
-        <section className='scan-container'>
-          <h2>Identify your Pill before scheduling</h2>
-          <ByImage setData={setData} />
-          <ByForm setData={setData} />
-          <OrAdd 
-            handleAddPill={handleAddPill}
-            handleAddPillReminders={handleAddPillReminders}
-          />
-        </section>
-      )}
-      </Suspense>
+    <Suspense fallback={<Spinner />}>
+      <Route
+        exact
+        path={`${match.url}`}
+        render={props => (
+          <IdentifyOptions {...props} addPill={addPill} setData={setData} />
+        )}
+      />
+      <Route
+        path={`${match.url}/results`}
+        render={props => (
+          <SearchResults searchResults={data} addPill={addPill} />
+        )}
+      />
+    </Suspense>
   );
 }
 
 export default connect(
   null,
   { addMed }
-)(ScanOrAdd);
+)(Identify);
