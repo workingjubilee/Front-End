b0a270d18793d84089d8f0ac9143325c50d935dd
Jacob Angulo <jacobangulo@hotmail.com>
2019-05-22 10:45:06 -0700

    created an array of team member data onjects to loop over in order to generate cards for each team member

---
+++ src/components/Landing/teamData.js
@@ -0,0 +1,66 @@
+export const team = [
+  {
+    image: require('./assets/OLU.jpg'),
+    name: 'OLU Durojaiye',
+    role: 'UX Designer',
+    gitHub: 'https://github.com/slimbugz',
+    linkedIn: 'https://www.linkedin.com/in/oluduro/',
+    medium: 'https://medium.com/@slimbugz'
+  },
+  {
+    image: require('./assets/Chris.jpg'),
+    name: 'Christopher Foster',
+    role: 'Web Developer',
+    gitHub: 'https://github.com/VictorGoic0',
+    linkedIn: 'https://www.linkedin.com/in/victor-m-goico/'
+  },
+  {
+    image: require('./assets/Victor.jpg'),
+    name: 'Victor Goico',
+    role: 'Web Developer',
+    gitHub: 'https://github.com/VictorGoic0',
+    linkedIn: 'https://www.linkedin.com/in/victor-m-goico/'
+  },
+  {
+    image: require('./assets/Jacob.jpg'),
+    name: 'Jacob Angulo',
+    role: 'Web Developer',
+    gitHub: 'https://github.com/jacobAngulo',
+    linkedIn: 'https://www.linkedin.com/in/jacob-angulo-723467175/'
+  },
+  {
+    image: require('./assets/Chance.jpg'),
+    name: 'Chance Embrey- Farquhar',
+    role: 'Web Developer',
+    gitHub: 'https://github.com/cembreyfarquhar',
+    linkedIn: 'https://www.linkedin.com/in/chance-embrey-farquhar/'
+  },
+  {
+    image: require('./assets/Jubilee.jpg'),
+    name: 'Jubilee Young',
+    role: 'Web Developer',
+    gitHub: 'https://github.com/workingjubilee',
+    linkedIn: 'https://www.linkedin.com/in/jubilee-young/'
+  },
+  {
+    image: require('./assets/Carlos.jpg'),
+    name: 'Carlos Gutierrez',
+    role: 'Data Scientist',
+    gitHub: 'https://github.com/carlos-gutier',
+    linkedIn: 'https://www.linkedin.com/in/carlos-gutierrez-304267162/'
+  },
+  {
+    image: require('./assets/Ed.jpg'),
+    name: 'Ed Chin',
+    role: 'Data Scientist',
+    gitHub: 'https://github.com/ed-chin-git',
+    linkedIn: 'https://www.linkedin.com/in/edgardo-chin-1557ba175/'
+  },
+  {
+    image: require('./assets/Mark.jpg'),
+    name: 'Mark Oliver',
+    role: 'Data Scientist',
+    gitHub: 'https://github.com/invegat',
+    linkedIn: 'https://www.linkedin.com/in/markoliverjdda/'
+  }
+];
