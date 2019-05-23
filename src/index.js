66eb270c161d226e242a7bdeacd7bbe404fc15f0
workingjubilee <workingjubilee@gmail.com>
2019-05-23 12:56:21 -0700

    Eliminates excess console logging outside dev

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+
 import App from './App';
 import AppWrapper from './AppWrapper';
+import './index.scss';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
